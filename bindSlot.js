1853877950822abe58380533ef34654a1c6e69da
Valerij Ivashchenko <likemusicdev@gmail.com>
2020-11-09 23:06:46 +0300

    Remove slot-watchers.

--- bindSlot.js
+++ bindSlot.js
@@ -17,56 +17,54 @@ export default function ({options}, render, name) {
 
     const unwatchers = {};
 
-    const initSlotsWatchers = function () {
-        updateWatcherBySlotName(name, this);
-        updatePreviousWatchersBySlotName(name, this);
+    /*
+        const initSlotsWatchers = function () {
+            updateWatcherBySlotName(name, this);
+            updatePreviousWatchersBySlotName(name, this);
 
-        function updateWatcherBySlotName(slotName, vm) {
-            unwatchIfWatcherExists(slotName);
-            setWatcherBySlotName(slotName, vm);
+            function updateWatcherBySlotName(slotName, vm) {
+                unwatchIfWatcherExists(slotName);
+                setWatcherBySlotName(slotName, vm);
 
 
-            function setWatcherBySlotName(slotName, vm) {
-                const r = vm.$options._slots[slotName].bind(vm, vm.$createElement);
-                unwatchers[slotName] = vm.$watch(
-                    r,
-                    t => {
-                        vm.$slots[name] = t;
-                        vm.$forceUpdate();
-                    },
-                    {immediate: true, deep: true}
-                );
-            }
-
-            function unwatchIfWatcherExists(slotName) {
-                const unwatcher = unwatchers[slotName];
-                if (!unwatcher) {
-                    return;
+                function setWatcherBySlotName(slotName, vm) {
+                    const r = vm.$options._slots[slotName].bind(vm, vm.$createElement);
+                    unwatchers[slotName] = vm.$watch(
+                        r,
+                        t => {
+                            vm.$slots[name] = t;
+                            vm.$forceUpdate();
+                        },
+                        {immediate: true, deep: true}
+                    );
                 }
 
-                unwatcher();
-            }
-        }
+                function unwatchIfWatcherExists(slotName) {
+                    const unwatcher = unwatchers[slotName];
+                    if (!unwatcher) {
+                        return;
+                    }
 
-        function updatePreviousWatchersBySlotName(slotName, vm) {
-            for (const [key, value] of Object.entries(vm.$options._slots)) {
-                if (key === name) {
-                    break;
+                    unwatcher();
                 }
-
-                updateWatcherBySlotName(key, vm);
             }
-        }
-    };
 
-    options.created = (options.created || []).concat(initSlotsWatchers);
+            function updatePreviousWatchersBySlotName(slotName, vm) {
+                for (const [key, value] of Object.entries(vm.$options._slots)) {
+                    if (key === name) {
+                        break;
+                    }
+
+                    updateWatcherBySlotName(key, vm);
+                }
+            }
+        };
+    */
+
+    // options.created = (options.created || []).concat(initSlotsWatchers);
     options.beforeUpdate = (options.beforeUpdate || []).concat(function () {
         const optionsSlots = this.$options._slots;
         const componentSlots = this.$slots;
-
-        if (Object.keys(optionsSlots).length === Object.keys(componentSlots).length) {
-            return;
-        }
 
         for (const [key, value] of Object.entries(optionsSlots)) {
             componentSlots[key] = optionsSlots[key].call(this, this.$createElement);
